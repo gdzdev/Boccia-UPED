e6a71e9731829df1d758ecffb4d2cc341199e7a2
Aleks Galdamez <aleksgaldamez2002@gmail.com>
2025-04-30 22:25:29 -0600

    initial commit

---
+++ src/main/java/org/gdzdev/spring/boccia/BocciaApplication.java
@@ -0,0 +1,13 @@
+package org.gdzdev.spring.boccia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BocciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gdzdev/spring/boccia/BocciaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gdzdev.spring.boccia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
